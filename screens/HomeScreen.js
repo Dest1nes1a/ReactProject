e68b77eb7d9534a4dfe7dfd5e70e41a5fbfa2cdf
Dest1nes1a <77435539+Dest1nes1a@users.noreply.github.com>
2022-01-19 11:57:55 +0700

    HomeScreen_ICON_HEADER-1/19/2022

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
-//import Ionicons from 'react-native-vector-icons/Ionicons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {
+  HeaderButtons,
+  HeaderButton,
+  Item,
+  HiddenItem,
+  OverflowMenu,
+} from 'react-navigation-header-buttons';
+
+const IoniconsHeaderButton = props => (
+  <HeaderButton IconComponent={Ionicons} iconSize={23} {...props} />
+);
 
 const HomeScreen = ({navigation}) => {
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+          <Item
+            title="menu"
+            iconName="menu"
+            onPress={() => alert('menu')}
+          />
+        </HeaderButtons>
+      ),
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+          <Item
+            title="Register"
+            iconName="person-add"
+            onPress={() => alert('Register')}
+          />
+        </HeaderButtons>
+      ),
+    });
+  }, [navigation]);
+
   return (
     <View style={styles.container}>
-     {/*  <Ionicons name='home-outline' size={30} color='#f4511e'/>*/}
+      <Ionicons name="home-outline" size={30} color="#f4511e" />
       <Text>หน้าหลัก</Text>
       <Button
         title="Go to About"
-        onPress={() => navigation.navigate('About',{email:'ju.siripong_st@tni.ac.th'})} //Para1 - Navigate ที่จะไป, Para2 - ค่าที่จะส่งไปแบบ Static
+        onPress={() =>
+          navigation.navigate('About', {email: 'ju.siripong_st@tni.ac.th'})
+        } //Para1 - Navigate ที่จะไป, Para2 - ค่าที่จะส่งไปแบบ Static
       />
     </View>
   );
